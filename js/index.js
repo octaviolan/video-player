ff36420964b73bd897cdd5774f0a0e496ecb9ad2
octaviolan <octavio.olan25@gmail.com>
2021-09-07 10:18:51 -0500

    Barra de progreso

--- js/index.js
+++ js/index.js
@@ -11,12 +11,16 @@ function handlePlay() {
   $video.play()
   $play.hidden = true
   $pause.hidden = false
+  $backward.hidden = false
+  $forward.hidden = false
   console.log('le diste click al boton de play')
 }
 
 function handlePause() {
   $video.pause()
   $pause.hidden = true
+  $backward.hidden = true
+  $forward.hidden = true
   $play.hidden = false
   console.log('le diste click al boton de pause')
 }
@@ -33,3 +37,28 @@ function handleForward() {
   $video.currentTime += 10
   console.log('10 seg para adelante', $video.currentTime)
 }
+
+$video.addEventListener('loadedmetadata', handleLoaded)
+$video.addEventListener('timeupdate', handleTimeUpDate)
+
+const $progress = document.querySelector('#progress')
+
+function handleLoaded() {
+  $progress.max = $video.duration
+  console.log('mi video ha cargado', $video.duration)
+}
+
+function handleTimeUpDate() {
+  $progress.value = $video.currentTime
+  // console.log('tiempo actual', $video.currentTime)
+}
+
+$progress.addEventListener('input', handleInput)
+
+function handleInput() {
+  $video.currentTime = $progress.value
+  console.log('el valor es', $progress.value)
+}
+
+
+
